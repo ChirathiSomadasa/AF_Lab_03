6eb1474a162b475b9369d31af739526601b913b2
ChirathiSomadasa <chirathisomadasa@gmail.com>
2025-03-02 11:44:09 +0530

    Add Promises

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 
+//Read file
 fs.readFile('file.txt', 'utf8', function (err, data) {
  if (err) throw err;
  console.log(data);
 });
 
+//Write to file
 fs.writeFile('file.txt', 'Hello World!', function (err) {
     if (err) throw err;
     console.log('File saved!');
 });
 
-
+// Creating a web server
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write('Hello World!');
  res.end();
 }).listen(8080);
 
-
+//Making an HTTP request
 https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
  let data = '';
  resp.on('data', (chunk) => {
@@ -32,5 +34,20 @@ https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
  console.log("Error: " + err.message);
 });
 
+//Using a module
 const myModule = require('./my_module.js');
 console.log(myModule.myFunction());
+
+//Promises
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+    resolve('Success!');
+    } else {
+    reject('Failure!');
+    }
+   });
+   myPromise.then((result) => {
+    console.log(result);
+   }).catch((error) => {
+    console.log(error);
+   });
